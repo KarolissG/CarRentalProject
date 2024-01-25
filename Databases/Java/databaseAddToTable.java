b5b76476661ace353775ab5a0c188cdb55426f86
KarolissG <144924449+KarolissG@users.noreply.github.com>
2024-01-25 09:40:59 +0000

    Added Java Files

---
+++ Databases/Java/databaseAddToTable.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class databaseAddToTable {
+    public static void main(String[] args) {
+        // database URL
+        final String DATABASE_URL = "jdbc:mysql://localhost:3306/carrental";
+        String name = "Commander Kurk"; // Values to add to table
+        Connection connection = null;
+        PreparedStatement pstat = null;
+        int i = 0;
+        try {
+            // establish connection to database
+            connection = DriverManager.getConnection(DATABASE_URL, "root", "");
+            // create Prepared Statement for updating data in the table
+            pstat = connection.prepareStatement("INSERT INTO login (name) VALUES (?)");
+            pstat.setString(1, name);
+            i = pstat.executeUpdate();
+            System.out.println(i + " record successfully updated in the table.");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                pstat.close();
+                connection.close();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+    } // end main
+} // end class
