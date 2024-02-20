93c5684b31af7f76ca8ed470db40e2131b62b854
KarolissG <144924449+KarolissG@users.noreply.github.com>
2024-02-20 22:02:20 +0000

    added updates to CRUD and test file

---
+++ Databases/Java/Java/databaseDelete.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class databaseDelete {
+    public static void main(String[] args) {
+        // database URL
+        final String DATABASE_URL = "jdbc:mysql://localhost:3306/carrental";
+        Connection connection = null;
+        PreparedStatement pstat = null;
+        int i = 0;
+        int customerID = 379; // value to remove
+
+        try {
+            // establish connection to database
+            connection = DriverManager.getConnection(DATABASE_URL, "root", "");
+            // create Prepared Statement for updating data in the table
+            pstat = connection.prepareStatement("DELETE FROM login WHERE UserID=?");
+            pstat.setInt(1, customerID);
+            i = pstat.executeUpdate();
+            System.out.println(i + " record successfully updated in the table.");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                pstat.close();
+                connection.close();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+    } // end main
+} // end class
